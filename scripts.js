fe877ee6233e84264c65d09f81a5ae12ed2c9674
Shivamvish7510 <shivamvishwakarma98105@gmail.com>
2024-09-22 16:13:31 +0530

    my portfolio website

---
+++ scripts.js
@@ -0,0 +1,17 @@
+var typed = new Typed(".typewriter-text",
+    {
+        strings: [" ",
+            "Web Developer",
+
+            " Designer",
+
+            "Editor",
+            
+            "Youtuber"
+        ],
+        typeSpeed: 100,
+        backSpeed: 70,
+        loop:true
+        
+    }
+)
